43418132b0270bc0f4b2ffeda872712a49c8048a
savaon <129174500+savaon@users.noreply.github.com>
2023-03-28 12:20:45 +0100

    Add files via upload
    That's just an example of my first little project which has been done using tutorial from - https://www.dataquest.io/blog/python-projects-for-beginners

--- Password generator.py
+++ Password generator.py
@@ -19,13 +19,16 @@ def shuffle(string):
 #Main program starts here
 import random
 uppercaseLetter1 = chr(random.randint(65,90)) #Generate a random Uppercase letter (based on ASCII code)
-uppercaseLetter2 = chr(random.randint(65,90)) #Generate a random Uppercase letter (based on ASCII code)
-lowercareLetter1 = chr(random.randint(65,90)) #Generate a radom chr from table but lower cases
+uppercaseLetter2 = chr(random.randint(65,90)) 
+lowercareLetter1 = chr(random.randint(65,90)) #Generate a radom chr from table but lower cases (based on ASCII code)
 lowercareLetter2 = chr(random.randint(65,90))
-digit1 = chr(random.randint(0,9))
+digit1 = chr(random.randint(0,9)) #random number (based on ASCII code)
 digit2 = chr(random.randint(0,9))
-punctuationSign1 = chr(random.randint(128,191))
+punctuationSign1 = chr(random.randint(128,191)) #random sign (based on ASCII code)
 punctuationSign2 = chr(random.randint(128,191))
+#sum of the prev functions
 password = uppercaseLetter1 + uppercaseLetter2 + lowercareLetter1 + lowercareLetter2 + digit1 + digit2 + punctuationSign1 + punctuationSign2
+#shuffle it
 shuffle(password)
+# output password
 print(password)
